e704b3b954b9b7b985bb9e77e55664b3dc2a234b
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-21 23:33:34 -0300

    alterando valor da variável birthYear e exibindo-a

--- fundamentos/secao-04-introducao-a-javascript/exercise-1.js
+++ fundamentos/secao-04-introducao-a-javascript/exercise-1.js
@@ -13,3 +13,9 @@ console.log(myName);
 console.log(birthCity);
 
 console.log(birthYear);
+
+// alterando valor atribuído à variável birthYer e exibindo-a com o console.log
+
+birthYear = 2030;
+
+console.log(birthYear);
