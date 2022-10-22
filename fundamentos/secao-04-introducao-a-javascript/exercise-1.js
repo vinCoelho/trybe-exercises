2e176756ded9a05bde945fcc90fa18482e35a32b
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-21 23:32:34 -0300

    exibindo variáveis e constante com console.log

--- fundamentos/secao-04-introducao-a-javascript/exercise-1.js
+++ fundamentos/secao-04-introducao-a-javascript/exercise-1.js
@@ -1,5 +1,15 @@
+// declarando variáveis e constante.
+
 let myName = 'Vinicius';
 
 const birthCity = 'Belo Horizonte';
 
 let birthYear = 1987;
+
+// exibindo variáveis e constante.
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
