612f77432562df5df1c52fe6c4f7f5799f9d7e49
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-23 21:25:13 -0300

    primeiros 5 programas, adição, subtração, multiplicação, divisão e módulo.

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -0,0 +1,26 @@
+// Nos exercícios de hoje, você vai fazer quinze pequenos programas. Todos trabalham a lógica condicional (if/else e switch/case), operadores aritméticos (+, -, *, /, %) e operadores lógicos (>, <, &&, ||). Para que consiga executar seus códigos, recomendamos que utilize a extensão Code Runner. Você pode ver mais sobre ela no conteúdo que fizemos sobre o uso do VS Code.
+
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+const a = 6;
+const b = 13;
+
+// Adição (a + b)
+
+console.log(a + b);
+
+// Subtração (a - b)
+
+console.log(a - b);
+
+// Multiplicação (a * b)
+
+console.log(a * b);
+
+// Divisão (a / b)
+
+console.log(a / b);
+
+// Módulo (a % b)
+
+console.log(a % b);
