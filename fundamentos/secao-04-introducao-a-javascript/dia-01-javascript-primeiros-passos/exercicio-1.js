b2884be7ddcb15c42c5808e510705b961532bb23
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-23 23:48:37 -0300

    cálculo lucro líquido

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -170,3 +170,27 @@ if (numer1 % 2 !== 0 || numer2 % 2 !== 0 || numer3 % 2 !== 0) {
 } else {
   console.log('false');
 }
+
+// Utilize if/else para escrever um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda, descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+
+// valorCustoTotal = valorCusto + impostoSobreOCusto;
+// lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+const valorCusto = 102.24;
+const valorVenda = 290.90;
+const valorImposto = valorCusto * 20 / 100;
+const valorCustoTotal = valorCusto + valorImposto;
+let lucro = valorVenda - valorCustoTotal;
+let lucroTotal;
+
+if (valorCustoTotal > 0 && valorVenda > 0 && lucro > 0) {
+  lucroTotal = lucro * 1000;
+  console.log('O lucro líquido é de: R$' + lucroTotal)
+} else {
+  console.log('Valor inválido.')
+}
