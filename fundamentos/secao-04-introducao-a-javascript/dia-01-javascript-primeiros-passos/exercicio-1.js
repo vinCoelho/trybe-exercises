12db788c7d0409ae8e73a685674d795ead14d036
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-23 22:06:23 -0300

    exercício verificação triângulo

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -56,3 +56,20 @@ if (num1 > 0) {
 } else {
   console.log('zero');
 }
+
+// 🚀 Utilize if/else para fazer um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
+const ang1 = 80;
+const ang2 = 40;
+const ang3 = 60;
+
+if ((ang1 + ang2 + ang3) == 180) {
+  console.log('true');
+} else if ((ang1 + ang2 + ang3) < 0) {
+  console.log('valores inválidos');
+} else {
+  console.log('false');
+}
