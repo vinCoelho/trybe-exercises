1f97bd5e8d3b7e13f0ed83a2d65c8a23ace9e4b9
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-23 21:37:59 -0300

    exercícios if/else para ver qual o maior número

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -24,3 +24,23 @@ console.log(a / b);
 // Módulo (a % b)
 
 console.log(a % b);
+
+// Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+if (a > b) {
+  console.log('O maior número é o: ' + a);
+} else {
+  console.log('O maior número é o: ' + b);
+}
+
+// Utilize if/else para fazer um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
+const c = 35;
+
+if (a > b && a > c) {
+  console.log('O maior dos três números é o (a): ' + a); 
+} else if (b > c) {
+  console.log('O maior dos três números é o (b): ' + b);
+} else {
+  console.log('O maior dos três números é o (c): ' + c);
+}
