11cb876c24bd0455a005bc74eeb1e838fcc9ec85
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-23 21:56:43 -0300

    exercício verificação positivo, negativo.

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -44,3 +44,15 @@ if (a > b && a > c) {
 } else {
   console.log('O maior dos três números é o (c): ' + c);
 }
+
+// Utilize if/else para fazer um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+const num1 = -7;
+
+if (num1 > 0) {
+  console.log('positive');
+} else if (num1 < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+}
