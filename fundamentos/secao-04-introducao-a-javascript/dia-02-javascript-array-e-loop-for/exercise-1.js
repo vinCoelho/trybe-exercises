545c84ee1f499e36b6336cbb4942a55ec4059388
Vinícius Coelho <viniciusraposo@gmail.com>
2022-10-24 22:28:04 -0300

    somando todos os valores do array

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercise-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercise-1.js
@@ -7,3 +7,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 }
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    sum = sum + numbers[index];
+}
+
+console.log('');
+console.log('A soma de todos os valores do array é: ' + sum);
